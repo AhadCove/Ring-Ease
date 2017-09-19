d628eeacb56a0baba0f134f4640b6fb109b680b0
BK Mac <ahadcove@gmail.com>
2017-09-19 18:45:27 -0400

    Port change

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import app from './app';
 import dotenv from 'dotenv'
 dotenv.config();
 
-const port = 7001;
+const port = process.env.PORT || 7001;
 
 app.listen(port, ()=>{
     console.log("Running on port ", port);
